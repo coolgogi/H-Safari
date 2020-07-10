7aa683ebe0c6a40233d4b57c63e34ed8e9e896f7
yeonyaily <yeonye330@gmail.com>
2020-07-10 16:26:50 +0900

    main U

--- h_safari/lib/main.dart
+++ h_safari/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
