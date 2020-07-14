3a292f755a4c5610bb4bd15b949c28ea683de1f5
yeonyaily <yeonye330@gmail.com>
2020-07-14 11:37:09 +0900

    0714 yejin1

--- h_safari/test/widget_test.dart
+++ h_safari/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:h_safari/lib/main.dart';
+import 'package:h_safari/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
