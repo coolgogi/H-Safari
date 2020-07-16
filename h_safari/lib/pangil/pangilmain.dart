a42acbee47a7a9864c01ef76edb096c91f26948e
yeonyaily <yeonye330@gmail.com>
2020-07-16 16:24:38 +0900

    YJ/0716/홈 페이지이동정리

--- h_safari/lib/pangil/pangilmain.dart
+++ h_safari/lib/pangil/pangilmain.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'chat.dart';
+import 'package:h_safari/chat/chat_list.dart';
 import 'write.dart';
 import 'category.dart';
-import 'mypage.dart';
+import 'package:h_safari/mypage/mypage.dart';
 
 
 class GI_MyApp extends StatefulWidget {
