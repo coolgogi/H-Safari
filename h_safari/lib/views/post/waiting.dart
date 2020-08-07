77e87653adab1e3bdfc7f8d48766043c84e2578f
coolgogi <41419516+coolgogi@users.noreply.github.com>
2020-08-07 16:48:54 +0900

    SH/0807/waiting list 최종

--- h_safari/lib/views/post/waiting.dart
+++ h_safari/lib/views/post/waiting.dart
@@ -82,114 +82,27 @@ class _WaitingState extends State<Waiting> {
       setState(() {
         waitingUserList = snapshots;
         print(
-            "we got the data + ${waitingUserList
-                .toString()} this is name  $documentID");
+            "we got the data + ${waitingUserList.toString()} this is name  $documentID");
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-//    // 이름 만드는 리스트
-//    var deco = ['귀여운', '잘생긴', '못생긴', '뚱뚱한', '착한', '한동대', '키가 큰', '키가 작은'];
-//    var animal = ['토끼', '강아지', '기린', '큰부리새', '김광일?', '코뿔소', '하마', '코끼리', '표범'];
-//    // 랜덤 변수 지정
-//    final _random = new Random();
-//    // 이름 값을 저장하는 리스트. (DB랑 연결되면 DB에 저장이 될 예정)
-//    List<String> names = [];
-//    for (int j = 1; j <= 10; j++) {
-//      // decoWord : 꾸미는 단어 랜덤으로 뽑기
-//      var decoWord = deco[_random.nextInt(deco.length)];
-//      // animal : 동물 단어 랜덤으로 뽑기
-//      var animalWord = animal[_random.nextInt(animal.length)];
-//      // userWord : 유저별 랜덤이름
-//      var userWord = decoWord + ' ' + animalWord;
-//
-//      // userWord를 리스트 names에 삽입. (DB랑 연결되면 DB에 추가할 예정)
-//      names.add(userWord);
-//    }
 
     return Scaffold(
         resizeToAvoidBottomPadding: false,
         appBar: appBar(context, '대기신청자'),
-        body:
-        StreamBuilder(
-          stream: Firestore.instance
-              .collection('post')
-              .document(documentID)
-              .collection("userList")
-              .orderBy("time")
-              .snapshots(),
-          builder: (context, snapshot) {
-            return
-//              Center(
-//              child:
-              GestureDetector(
-                  child: ListView.builder(
-                    // shrinkWrap : (무슨 역할인지,, 모르겠어요)
-                      shrinkWrap: true,
-                      // itemCount : userName이 저장된 리스트 names의 길이만큼 다이어로그에 보여준다.
-//                        itemCount: names.length,
-                      itemCount: snapshot.data.documents.length,
-                      // itemBuilder : userName이 보여지는 공간
-                      itemBuilder: (BuildContext context, int index) {
-                        return Padding(
-                          // padding : padding을 아래 10px 지정
-                            padding: const EdgeInsets.only(
-                                left: 40, right: 40, top: 20),
-                            child: Container(
-                              // ListTile의 스타일 지정
-                              decoration: BoxDecoration(
+        body: Container(
+          child: waitingList(),
+        ),
 
-                                // 모서리 둥근 정도
-                                  borderRadius: BorderRadius.all(
-                                      Radius.circular(10)),
-                                  // 배경색
-                                  color: Colors.green[200]),
-                              height: 55,
-                              width: double.maxFinite,
-                              child: //Text('$test[index]'),
-                              ListTile(
-//                                  title: Text('[' +
-//                                      (index + 1).toString() +
-//                                      '] ' +
-//                                      names[index])),
-                                title: Text(snapshot.data.documents[index]
-                                    .data['sendBy']),
-                                subtitle: Text(snapshot.data.documents[index]
-                                    .data['time']),
-                                leading: GestureDetector(
-//                                  behavior: HitTestBehavior.translucent,
-                                  onTap: () {
-
-                                  },
-                                  child: Container(
-                                    width: 20,
-                                    height: 48,
-//                                    padding: EdgeInsets.symmetric(vertical: 4.0),
-//                                    alignment: Alignment.center,
-//                                    child: CircleAvatar(),
-                                  ),
-                                ),
-
-                              ), //ListTile
-                            ) //Container
-                        ); //Padding
-                      } //itembuilder
-                  ) //List.view builder
-              ); //GestureDetector
-//            ); //center
-          }, //builder
-        ) //streamBuilder
     ); //Scaffold
   }
 
   Widget waitingTile(String sendBy, String time, String documentID) {
     fp = Provider.of<FirebaseProvider>(context);
-    userEmail = fp
-        .getUser()
-        .email
-        .toString();
+    userEmail = fp.getUser().email.toString();
 
     return FlatButton(
         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
@@ -198,22 +111,6 @@ class _WaitingState extends State<Waiting> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            //            Container(
-            //              padding: EdgeInsets.all(8),
-            //              decoration: BoxDecoration(
-            //                  color: Colors.white,
-            //                  shape: BoxShape.circle,
-            //                  boxShadow: [
-            //                    BoxShadow(
-            //                      color: Colors.black38,
-            //                      spreadRadius: 0.5,
-            //                    )
-            //                  ]),
-            //              child: Icon(
-            //                getMyIcon(type), // 아이콘 종류
-            //                color: Colors.brown, // 아이콘 색
-            //              ),
-            //            ), // 아이콘
             SizedBox(
               width: 14,
             ), // 아이콘과 글자들 사이의 박스 삽입
@@ -230,19 +127,11 @@ class _WaitingState extends State<Waiting> {
                   time, // 알람 내용
                   style: TextStyle(fontSize: 13), // 알림 내용 스타일 지정
                 ),
-                // 아이콘과 글자들 사이의 박스 삽입
-                //                Text(
-                //                  time, // 시간
-                //                  style: TextStyle(
-                //                      fontSize: 10, color: Colors.black45), // 시간 스타일 지정
-                //                ),
               ],
             ),
           ],
         ),
         onPressed: () {
-          //          showDocument(postID);
-          //          DatabaseMethods().updateUnreadAlram(userEmail, documentID);
           sendMessage(sendBy);
         });
   }
