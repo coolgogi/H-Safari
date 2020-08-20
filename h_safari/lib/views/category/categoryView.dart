051e8729cff94927b32c3315f97e60806b8c0fd0
yeonyaily <yeonye330@gmail.com>
2020-08-20 16:05:30 +0900

    YJ/0820/코드정리

--- h_safari/lib/views/category/categoryView.dart
+++ h_safari/lib/views/category/categoryView.dart
@@ -1,48 +1,31 @@
-// 기본 import
 import 'package:flutter/material.dart';
-
-// widget import
 import 'package:h_safari/widget/widget.dart';
-
-//
 import 'package:h_safari/helpers/firebase_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:h_safari/views/post/post.dart';
-import 'dart:io';
 import 'package:custom_switch/custom_switch.dart';
 
-class categoryView extends StatefulWidget {
+class CategoryView extends StatefulWidget {
   final String select;
 
-  categoryView({this.select});
+  CategoryView({this.select});
 
   @override
-  _categoryViewState createState() => _categoryViewState(select: select);
+  _CategoryViewState createState() => _CategoryViewState(select: select);
 }
 
-class _categoryViewState extends State<categoryView> {
+class _CategoryViewState extends State<CategoryView> {
   final String select;
 
-  _categoryViewState({this.select});
+  _CategoryViewState({this.select});
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   String email;
   FirebaseProvider fp;
   var status = true;
-  File _image;
-  final String fnName = "name";
-  final String fnDescription = "description";
-  final String fnDatetime = "datetime";
-  final String fnImageUrl = 'imageUrl';
-  final String fnPrice = 'price';
-  final String fnCategory = 'category';
-  final String fnHow = 'how'; //거래유형
-  final String fnEmail = 'email';
-  final String fnClose = 'close';
   var _isSwitchedNum = true;
-
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +45,7 @@ class _categoryViewState extends State<categoryView> {
       child: StreamBuilder<QuerySnapshot>(
         stream: Firestore.instance
             .collection('post')
-            .orderBy(fnDatetime, descending: true)
+            .orderBy("datetime", descending: true)
             .snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) return Text("Error: ${snapshot.error}");
@@ -72,35 +55,27 @@ class _categoryViewState extends State<categoryView> {
             default:
               return ListView(
                 children:
-                snapshot.data.documents.map((DocumentSnapshot document) {
-                  Timestamp ts = document[fnDatetime];
-                  String dt = timestampToStrDateTime(ts);
-                  String _profileImageURL = document[fnImageUrl];
-                  String postCategory = document[fnCategory];
-//                  postCategory.replaceAll("/", "");
-                  bool close = document[fnClose];
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  String postCategory = document['category'];
+                  bool close = document['close'];
 
                   if (!(postCategory == select))
                     return Container();
                   else {
-                    return _isSwitchedNum == true ?
-                    InkWell(
-                      // Read Document
-                        onTap: () {
-                          showReadPostPage(document);
-                        },
-                        child: postTile(context, document)
-                    )
-                        :  close == false && _isSwitchedNum == false ?
-                    InkWell(
-                      // Read Document
-                        onTap: () {
-                          showReadPostPage(document);
-                        },
-                        child: postTile(context, document)
-                    )
-                        :Container();
-                  } //Card
+                    return _isSwitchedNum == true
+                        ? InkWell(
+                            onTap: () {
+                              showReadPostPage(document);
+                            },
+                            child: postTile(context, document))
+                        : close == false && _isSwitchedNum == false
+                            ? InkWell(
+                                onTap: () {
+                                  showReadPostPage(document);
+                                },
+                                child: postTile(context, document))
+                            : Container();
+                  }
                 }).toList(),
               );
           }
@@ -114,10 +89,13 @@ class _categoryViewState extends State<categoryView> {
         .toString();
   }
 
-  //문서 읽기 (Read)
   void showReadPostPage(DocumentSnapshot doc) {
     _scaffoldKey.currentState..hideCurrentSnackBar();
-    Navigator.push(context, MaterialPageRoute(builder: (context) => email == doc['email'] ? Post(doc, true) : Post(doc, false)));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                email == doc['email'] ? Post(doc, true) : Post(doc, false)));
   }
 
   Widget appBarSelect(BuildContext context, String title) {
@@ -137,35 +115,32 @@ class _categoryViewState extends State<categoryView> {
         padding: const EdgeInsets.only(left: 40.0),
         child: Center(
             child: Text(
-              '$title',
-              style: TextStyle(
-                  color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
-            )),
+          '$title',
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+        )),
       ),
       actions: <Widget>[
-
-    Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Transform.scale(
-        scale: 0.65,
-          child: CustomSwitch(
-            activeColor: Colors.green,
-            value: status,
-            onChanged: (value) {
-              setState(() {
-                status = value;
-                _isSwitchedNum = value;
-              });
-            },
-          ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Transform.scale(
+              scale: 0.65,
+              child: CustomSwitch(
+                activeColor: Colors.green,
+                value: status,
+                onChanged: (value) {
+                  setState(() {
+                    status = value;
+                    _isSwitchedNum = value;
+                  });
+                },
+              ),
+            ),
+            Text('마감', style: TextStyle(fontSize: 14, color: Colors.black38)),
+          ],
         ),
-        Text('마감', style : TextStyle(fontSize: 14, color : Colors.black38)),
       ],
-    ),
-
-      ],
-
     );
   }
 }
--- h_safari/lib/views/home/search.dart
+++ h_safari/lib/views/home/search.dart
@@ -14,17 +14,7 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-  final String fnName = "name";
-  final String fnDescription = "description";
-  final String fnDatetime = "datetime";
-  final String fnImageUrl = 'imageUrl';
-  final String fnPrice = 'price';
-  final String fnCategory = 'category';
-  final String fnHow = 'how'; //거래유형
-  final String fnEmail = 'email';
-  final String fnClose = 'close';
   var _isSwitchedNum = true;
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   var status = true;
   FirebaseProvider fp;
   String currentEmail;
@@ -36,14 +26,9 @@ class _SearchState extends State<Search> {
 
   final key = new GlobalKey<ScaffoldState>();
   final TextEditingController _searchQuery = new TextEditingController();
-  List<String> _list;
 
-  bool wantToSeeFinished = true; //마감된글 볼지말지
+  bool wantToSeeFinished = true;
 
-//  List<postItem> postList;
-
-//  List<String> titles;
-//  List<String> Descriptions;
 
   bool _IsSearching;
   String _searchText = "";
@@ -83,7 +68,7 @@ class _SearchState extends State<Search> {
           child: StreamBuilder<QuerySnapshot>(
             stream: Firestore.instance
                 .collection('post')
-                .orderBy(fnDatetime, descending: true)
+                .orderBy("datetime", descending: true)
                 .snapshots(),
             builder:
                 (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -96,20 +81,13 @@ class _SearchState extends State<Search> {
                       child: ListView(
                         padding: EdgeInsets.symmetric(vertical: 8.0),
                         children:
-//                          _IsSearching ? _buildSearchList() :
                         snapshot.data.documents
                             .map((DocumentSnapshot document) {
-                          String title = document[fnName];
-                          String postDes = document[fnDescription];
-                          Timestamp ts = document[fnDatetime];
-                          String dt = timestampToStrDateTime(ts);
-                          String _profileImageURL = document[fnImageUrl];
-                          String postCategory = document[fnCategory];
-                          bool close = document[fnClose];
-                          priceComma = document[fnPrice].replaceAllMapped(
+                          String title = document["name"];
+                          bool close = document['close'];
+                          priceComma = document['price'].replaceAllMapped(
                               RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
                               (match) => '${match[1]},');
-
                           if (!_IsSearching)
                             return Container();
                           else if (!title.contains(_searchQuery.text))
@@ -117,136 +95,17 @@ class _SearchState extends State<Search> {
                           else {
                             return _isSwitchedNum == true
                                 ? InkWell(
-                                    // Read Document
                                     onTap: () {
                                       showReadPostPage(document);
                                     },
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                          border: Border(
-                                              bottom: BorderSide(
-                                                  color: Colors.black12))),
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 15),
-                                      child: Row(
-                                        crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          // 사진
-                                          listPhoto(context, document),
-                                          SizedBox(
-                                            width: 15,
-                                          ),
-                                          Container(
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width /
-                                                20 *
-                                                11,
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: <Widget>[
-                                                // 게시물 제목
-                                                Text(
-                                                  document[fnName],
-                                                  style: TextStyle(
-                                                    color: Colors.black,
-                                                    fontSize: 20,
-                                                    fontWeight: FontWeight.w400,
-                                                  ),
-                                                ),
-                                                // 게시물 가격
-                                                Text(
-                                                  '$priceComma원',
-                                                  style: TextStyle(
-                                                      color: Colors.black,
-                                                      fontSize: 18,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                                // 게시물 내용 (3줄까지만)
-                                                Text(
-                                                  document[fnDescription],
-                                                  style: TextStyle(
-                                                    color: Colors.black54,
-                                                    fontSize: 12,
-                                                  ),
-                                                  maxLines: 3,
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
+                                    child: postTile(context, document),
                                   )
                                 : close == false && _isSwitchedNum == false
                                     ? InkWell(
-                                        // Read Document
                                         onTap: () {
                                           showReadPostPage(document);
                                         },
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                              border: Border(
-                                                  bottom: BorderSide(
-                                                      color: Colors.black12))),
-                                          padding: const EdgeInsets.symmetric(
-                                              vertical: 15),
-                                          child: Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: <Widget>[
-                                              // 사진
-                                              listPhoto(context, document),
-                                              SizedBox(
-                                                width: 15,
-                                              ),
-                                              Container(
-                                                width: MediaQuery.of(context)
-                                                        .size
-                                                        .width /
-                                                    20 *
-                                                    11,
-                                                child: Column(
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
-                                                  children: <Widget>[
-                                                    // 게시물 제목
-                                                    Text(
-                                                      document[fnName],
-                                                      style: TextStyle(
-                                                        color: Colors.black,
-                                                        fontSize: 20,
-                                                        fontWeight:
-                                                            FontWeight.w400,
-                                                      ),
-                                                    ),
-                                                    // 게시물 가격
-                                                    Text(
-                                                      document[fnPrice] + '원',
-                                                      style: TextStyle(
-                                                          color: Colors.black,
-                                                          fontSize: 18,
-                                                          fontWeight:
-                                                              FontWeight.bold),
-                                                    ),
-                                                    // 게시물 내용 (3줄까지만)
-                                                    Text(
-                                                      document[fnDescription],
-                                                      style: TextStyle(
-                                                        color: Colors.black54,
-                                                        fontSize: 12,
-                                                      ),
-                                                      maxLines: 3,
-                                                    ),
-                                                  ],
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
+                                        child: postTile(context, document),
                                       )
                                     : Container();
                           }
@@ -266,7 +125,6 @@ class _SearchState extends State<Search> {
         .toString();
   }
 
-//  문서 읽기 (Read)
   void showReadPostPage(DocumentSnapshot doc) {
     fp = Provider.of<FirebaseProvider>(context);
     currentEmail = fp.getUser().email.toString();
