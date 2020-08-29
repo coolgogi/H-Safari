69f75fc385ee24ab0a8f13b1593e90e3d0cab588
coolgogi <suhyun926@naver.com>
2020-08-29 23:35:09 +0900

    SH/0829/signUp 수정2

--- h_safari/lib/views/login/signUp.dart
+++ h_safari/lib/views/login/signUp.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:h_safari/views/mypage/terms_of_use.dart';
-import '../../helpers/firebase_provider.dart';
+import 'package:h_safari/views/mypage/privacyPolicy.dart';
+import 'package:h_safari/helpers/firebase_provider.dart';
+
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
@@ -252,7 +255,7 @@ class _SignUpState extends State<SignUp> {
                               Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => Terms()));
+                                      builder: (context) => PrivacyPolicy()));
                             },
                           ),
                           Row(
